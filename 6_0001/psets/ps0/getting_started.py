ee75a14eba52d4d1a62c3d5165f23dad72fca3f9
John Jones <johnny.l.jones.iv@gmail.com>
2019-08-24 14:56:13 -0400

    .gitignore fix, i think

---
+++ 6_0001/psets/ps0/getting_started.py
@@ -0,0 +1,10 @@
+"""
+MIT OCW 6.0001 Fall 2016
+Problem Set 0
+"""
+import math
+
+x = int(input('enter x:')) # get user str input for x then cast str to int
+y = int(input('enter y:')) # get user str input for y then cast str to int
+print('x**y =', x**y)
+print('log2(x) =', math.log2(x))
---
+++ 6_0001/psets/ps0/myLect1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@title: myLect1.py
+"""
+import random
+
+x = random.randint(16,272)
+print(x)
+
+# finding the square root of x
