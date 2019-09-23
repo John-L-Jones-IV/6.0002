a33b7cfc48ae58f2a96226621c7e36c9ee0650e7
John Jones <johnny.l.jones.iv@gmail.com>
2019-09-22 22:30:19 -0400

    ps4 complete

---
+++ 6_0001/psets/ps4/ps4a.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# Problem Set 4A
+# Name: John L. Jones
+# Collaborators:
+# Time Spent: x:xx
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+    
+    # BASE CASE #
+    if len(sequence) <= 1:
+        return [sequence]
+    
+    # RECURSIVE CASE #
+    else:
+        # get permutations of all but the first letter
+        perms_sub = get_permutations(sequence[1:])
+        perms = []
+
+        for i in range(len(sequence)): # for each letter in sequence
+            for perm in perms_sub:
+                perms.append(perm[:i] + sequence[0] + perm[i:])
+
+        return perms
+
+if __name__ == '__main__':
+#    # Put three example test cases here (for your sanity, limit your inputs
+#    to be three characters or fewer as you will have n! permutations for a 
+#    sequence of length n)
+
+    example_input = 'a'
+    print('Input:', example_input)
+    print('Expected Output:', ['a'])
+    print('Actual Output:  ', get_permutations(example_input))
+    
+    example_input = 'ab'
+    print('')
+    print('Input:', example_input)
+    expected_output = ['ab', 'ba']
+    expected_output.sort()
+    actual_output = get_permutations(example_input)
+    actual_output.sort()
+    print('Expected Output:', expected_output)
+    print('Actual Output:  ', actual_output) 
+
+    example_input = 'abc'
+    print('')
+    print('Input:', example_input)
+    expected_output = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    expected_output.sort()
+    actual_output = get_permutations(example_input)
+    actual_output.sort()
+    print('Expected Output:', expected_output)
+    print('Actual Output:  ', actual_output)
+
+    example_input = 'cat'
+    print('')
+    print('Input:', example_input)
+    expected_output = ['cat', 'act', 'atc', 'tca', 'cta', 'tac']
+    expected_output.sort()
+    actual_output = get_permutations(example_input)
+    actual_output.sort()
+    print('Expected Output:', expected_output)
+    print('Actual Output:  ', actual_output)
+
+    example_input = 'zyx'
+    print('')
+    print('Input:', example_input)
+    expected_output = ['zyx', 'zxy', 'yzx', 'yxz', 'xyz', 'xzy']
+    expected_output.sort()
+    actual_output = get_permutations(example_input)
+    actual_output.sort()
+    print('Expected Output:', expected_output)
+    print('Actual Output:  ', actual_output)
